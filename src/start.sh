a3a98ff010c9f2d36dded3ed53532b81e12b72ff
gaudi <craig@resin.io>
2016-01-29 17:48:38 +0000

    kill watchdog, when container is killed

--- src/start.sh
+++ src/start.sh
@@ -1,6 +1,14 @@
-if [ "$INITSYSTEM" == "on" ]; then
-  # load watchdog kernal module
-  modprobe bcm2708_wdog
-  echo "Beware this system is being protected by a watchdog"
-fi
-# Run you app logic here
+# load watchdog kernal module
+modprobe bcm2708_wdog
+echo "Beware this system is being protected by a watchdog"
+
+# Run your application here
+# node app.js
+
+# unmount hardware watchdog when the container is killed
+trap 'modprobe -r bcm2708_wdog' SIGTERM
+# wait indefinitely
+while :
+do
+	sleep 1
+done
