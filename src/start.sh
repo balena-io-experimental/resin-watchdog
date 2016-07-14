08d83335e83d249a53bc349feeb2a6abcc07b331
Pagan Gazzard <pjgazzard@googlemail.com>
2016-07-13 17:00:46 -0700

    Disable the hardware watchdog when exiting, rather than just unloading the module.

--- src/start.sh
+++ src/start.sh
@@ -1,12 +1,20 @@
-# load watchdog kernal module
+# load watchdog kernel module
 modprobe bcm2708_wdog
 echo "Beware this system is being protected by a watchdog"
 
 # Run your application here
 # node app.js
 
-# unmount hardware watchdog when the container is killed
-trap 'modprobe -r bcm2708_wdog' SIGTERM
+
+disable_watchdog () {
+	# disable the watchdog hardware
+	echo V > /dev/watchdog
+	# unload the watchdog kernel module
+	modprobe -r bcm2708_wdog
+}
+
+# disable the watchdog when the container is killed
+trap disable_watchdog SIGTERM
 # wait indefinitely
 while :
 do
